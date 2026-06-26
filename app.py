901e59805f2367f064e0686252ba987675b9a3a3
Noel K <nbkurian11@gmail.com>
2026-06-25 23:26:06 -0400

    switch to alpga vantage api

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
 from flask import Flask, jsonify, render_template
+import requests
+import os
+from dotenv import load_dotenv
 
-import yfinance as yf
+load_dotenv()
 
 app = Flask(__name__)
+
+API_KEY = os.getenv("ALPHA_VANTAGE_KEY")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/stock/<ticker>")
-
 def get_stock(ticker):
-
     try:
-        tick = yf.Ticker(ticker)
-        info = tick.info
-
+        url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={API_KEY}"
+        response = requests.get(url)
+        data = response.json()
+        quote = data["Global Quote"]
 
         return jsonify({
             "ticker": ticker.upper(),
-            "currency": info["currency"],
-            "price": info["currentPrice"],
-            "change": info["regularMarketChange"],
-            "change_percent": info["regularMarketChangePercent"],
-            "52w_low": info["fiftyTwoWeekLow"],
-            "52w_high": info["fiftyTwoWeekHigh"]
+            "price": float(quote["05. price"]),
+            "change": float(quote["09. change"]),
+            "change_percent": float(quote["10. change percent"].replace("%", "")),
+            "day_low": float(quote["04. low"]),
+            "day_high": float(quote["03. high"])
         })
-    except KeyError:
-        return jsonify({"error": "Invalid ticker symbol"}), 404
 
-
+    except Exception as e:
+        return jsonify({"error": "Invalid ticker or data unavailable"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
